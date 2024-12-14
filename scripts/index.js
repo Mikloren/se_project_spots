dd02cebb8179f4e287d18a673c9ac045de476787
MichaelLorenzo <micheallowkey@gmail.com>
2024-12-14 16:17:27 -0500

    1.v.0

--- scripts/index.js
+++ scripts/index.js
@@ -40,7 +40,7 @@ const editModalDescriptionInput = editModal.querySelector(
 
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = cardModal.querySelector(".modal__form");
-const cardSubmitBtn = cardModal.querySelector(".modal__button");
+const cardSubmitBtn = cardModal.querySelector(".modal__submit-btn");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
@@ -73,9 +73,9 @@ function handleAddCardSubmit(evt) {
   const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
   const cardElement = getCardElement(inputValues);
   cardsList.prepend(cardElement);
+  evt.target.reset();
   disableButton(cardSubmitBtn);
   closeModal(cardModal);
-  evt.target.reset();
 }
 
 function getCardElement(data) {
@@ -112,6 +112,10 @@ function getCardElement(data) {
 profileEditButton.addEventListener("click", () => {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
+  resetValidation(editFormElement, [
+    editModalNameInput,
+    editModalDescriptionInput,
+  ]);
   openModal(editModal);
 });
 
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,13 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
   const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = errorMsg;
-  inputEl.classList.add(".modal__input_type_error");
+  inputEl.classList.add("modal__input_type_error");
 };
 
 const hideInputError = (formEl, inputEl) => {
   const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = "";
-  inputEl.classList.remove(".modal__input_type_error");
+  inputEl.classList.remove("modal__input_type_error");
 };
 
 const checkInputValidity = (formEl, inputEl) => {
@@ -29,23 +29,29 @@ const toggleButtonState = (inputList, buttonElement) => {
     disableButton(buttonElement);
   } else {
     buttonElement.disabled = false;
-    buttonElement.classList.remove(".modal__submit-btn_type_disabled");
+    buttonElement.classList.remove("modal__submit-btn_type_disabled");
   }
 };
 
 const disableButton = (buttonElement) => {
   buttonElement.disabled = true;
-  buttonElement.classList.add(".modal__submit-btn_type_disabled");
+  buttonElement.classList.add("modal__submit-btn_type_disabled");
 };
 
-const setEventListeners = (formEl) => {
-  const inputList = Array.form(formEl.querySelectorAll(".modal__input"));
-  const buttonElement = formEl.querySelector(".modal__submit-btn");
+const resetValidation = (formEl, inputList) => {
+  inputList.forEach((input) => {
+    hideInputError(formEl, input);
+  });
+};
+
+const setEventListener = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll("modal__input"));
+  const buttonElement = formEl.querySelector("modal__submit-btn");
 
   toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
-    inputElement.addEventListeners("input", function () {
+    inputElement.addEventListener("input", function () {
       checkInputValidity(formEl, inputElement);
       toggleButtonState(inputList, buttonElement);
     });
@@ -53,9 +59,9 @@ const setEventListeners = (formEl) => {
 };
 
 const enableValidation = () => {
-  const formList = document.querySelectorAll(".modal__form");
+  const formList = document.querySelectorAll("modal__form");
   formList.forEach((formEl) => {
-    setEventListeners(formEl);
+    setEventListener(formEl);
   });
 };
 
